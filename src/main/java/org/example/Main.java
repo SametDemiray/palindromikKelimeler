b9f642c374e7f1c475807782744954129a2abdf8
SametDemiray <sametdemiray@yahoo.com>
2024-03-14 03:51:20 +0300

    Palindromik Kelimeler

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Scanner;
+
+// Baştan ve sondan yazımları / okunuşları aynı olan kelimelere "Palindromik (palindrom)kelime"ler denir. Buna göre
+// klavyeden küçük harflerle girilen kelimenin palindromik olup olmadığını söyleyen program.
+// örn. kek,mum,küçük,kök
+public class Main {
+    public static void main(String[] args) {
+
+        String k;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Kelime : ");
+        k = scanner.nextLine();
+       // String kt = new StringBuffer(k).reverse().toString();
+        String kt = new  StringBuilder(k).reverse().toString();
+        if (k.equals(kt))
+            System.out.println("Palindromik Kelimedir.");
+        else System.out.println("Palindromik Kelime Değildir");
+    }
+}
